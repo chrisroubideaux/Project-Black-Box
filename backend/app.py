8c482a1bd7ae8dddcfe34118bb89b7dd3d32c35e
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2025-06-21 01:09:58 -0500

    updated txt.requiremnts

--- backend/app.py
+++ backend/app.py
@@ -74,5 +74,12 @@ def hello():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  
-    app.run(debug=True)
+        db.create_all()
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
+#if __name__ == '__main__':
+#    with app.app_context():
+#        db.create_all()  
+#    app.run(debug=True)
